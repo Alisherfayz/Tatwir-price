81c720d32d9462e7dfac948a03637f40a315fa64
Alisherfayz <mahammadjonov7777@gmail.com>
2024-04-21 20:07:40 +0500

    adaaaaaa

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Tatwir price',
+      title: 'Tatwir priceAAA11',
       home: MyHomePage(),
     );
   }
